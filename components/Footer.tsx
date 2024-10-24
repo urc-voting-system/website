d933a7c613ea2c2431a7acc95b354a0bad3683fc
Senyo-Joel <116897993+Senyoj@users.noreply.github.com>
2024-10-24 02:08:09 +0000

    update gallery features

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
               Ready to dive into the world of robotics
             </h1>
             <p className="text-sm py-5">
-              Join the UENR Robotics Club and be part of a team that's pushing
+              Join the UENR Robotics Club and be part of a team that is pushing
               the boundaries of technology and innovation
             </p>
           </div>
--- components/ui/flip-words.tsx
+++ components/ui/flip-words.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useCallback, useEffect, useState } from "react";
-import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export const FlipWords = ({
